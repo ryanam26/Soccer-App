e7cacae72514fcfc1929fc1e4a251df42d6e0994
Lenin Luque <xleninx@gmail.com>
2014-05-12 13:29:05 -0430

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SoccerApp::Application.config.secret_key_base = 'c4eae0500e0bb0b5543fdf6893451cf7607a372ac3dffe6ebfd0f93285255104feb9fdfc66081b7255036662bda70c4e9e2263ea6921ad305f021fb6294db289'
