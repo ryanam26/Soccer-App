7c9d9c9221a32514368a71ad713dbb2adc5dea36
Lenin Luque <xleninx@gmail.com>
2014-05-23 15:45:11 -0430

    fix bugs

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,22 +4,28 @@ class Team < ActiveRecord::Base
 
   def team_average_time(test_id)
     sql = "select to_char(to_timestamp(avg(s.value)) AT TIME ZONE 'UTC','HH24:MI:SS') as average from scores s, teams_users ts where s.test_id = #{test_id} and s.user_id = ts.user_id and ts.team_id = #{id};"
-    self.connection.execute(sql).to_a[0]["average"]
+    connection.execute(sql).to_a[0]["average"]
   end
 
   def team_average_numeric(test_id)
     sql = "select avg(s.value) as average from scores s, teams_users ts where s.test_id = #{test_id} and s.user_id = ts.user_id and ts.team_id = #{id};"
-    self.connection.execute(sql).to_a[0]["average"].to_f.round(2)
+    connection.execute(sql).to_a[0]["average"].to_f.round(2)
   end
 
   def team_system_rank_time(test_id)
     sql = "select * from (select row_number() OVER (order by to_char(to_timestamp(avg(s.value)) AT TIME ZONE 'UTC','HH24:MI:SS') ASC) as pos, ts.team_id, to_char(to_timestamp(avg(s.value)) AT TIME ZONE 'UTC','HH24:MI:SS') as average from scores s, teams_users ts where s.test_id = #{test_id} and s.user_id = ts.user_id group by ts.team_id) as h where team_id = #{id}"
-    self.connection.execute(sql).to_a[0]["pos"]
+    pos = connection.execute(sql)
+    if pos.count > 0
+      pos.to_a[0]["pos"]
+    end
   end
 
   def team_system_rank_numeric(test_id)
     sql = "select * from (select row_number() OVER (order by avg(value) DESC) as pos, ts.team_id, avg(s.value) from scores s, teams_users ts where s.test_id = #{test_id} and s.user_id = ts.user_id group by ts.team_id) as h where team_id = #{id};"
-    self.connection.execute(sql).to_a[0]["pos"]
+    pos = connection.execute(sql)
+    if pos.count > 0
+      pos.to_a[0]["pos"]
+    end
   end
 
 end
