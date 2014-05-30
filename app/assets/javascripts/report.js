b34806d5e4bde70a90e71f513ae63f844f3230c1
Lenin Luque <xleninx@gmail.com>
2014-05-29 21:42:53 -0430

    test

--- app/assets/javascripts/report.js
+++ app/assets/javascripts/report.js
@@ -30,8 +30,8 @@ $(document).ready(function() {
             }
             ],
             
-        },"aLengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]],
-        "iDisplayLength": -1
+        },
+        "aLengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]]
       });
     $(".DTTT").addClass("pull-right");    
 
