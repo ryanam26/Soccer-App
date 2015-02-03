02ff771032a8bdbea881c628c998de98df2b7468
Dave <d4park@gmail.com>
2015-02-03 15:10:47 -0500

    Minor changes to the code to make it work properly on the first run.

--- Gemfile
+++ Gemfile
@@ -53,6 +53,10 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+group :test do
+  gem 'sqlite3'
+end
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20140529232356) do
 
   create_table "accounts", force: true do |t|
     t.string   "name"
+    t.integer  "status"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "status"
   end
 
   create_table "accounts_users", force: true do |t|
