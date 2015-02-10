3f7394273f95e8be5951b7c1297ee9b559e132e4
Dave <d4park@gmail.com>
2015-02-09 19:32:31 -0500

    Adding a callback to set any new accounts to active upon creation.

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,10 +3,19 @@ class Account < ActiveRecord::Base
   has_many :locations
   has_and_belongs_to_many :users
 
-  extend EnumerateIt
-  has_enumeration_for :status, :with => Status, :create_helpers => true
-  
   scope :active, lambda{where(:status => Status::ACTIVE)}
   scope :inactive, lambda{where(:status => Status::INACTIVE)}
 
+  extend EnumerateIt
+  has_enumeration_for :status, :with => Status, :create_helpers => true
+  
+  before_create :activate
+  
+  def activate
+    status = Status::ACTIVE
+  end
+  
+  def deactivate
+    status = Status::DEACTIVE
+  end
 end
