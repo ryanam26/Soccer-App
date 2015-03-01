8fc09ab904136b9a663ff1c7c51a823888b65e00
Dave <d4park@gmail.com>
2015-03-01 11:19:59 -0500

    Attempting to make dynamic forms work on the coach page.  Failing miserably so far.

---
+++ app/assets/javascripts/coach_profile.js
@@ -0,0 +1,14 @@
+$(function(){
+  // $(var populate_tests = function(category_id) {
+        // if (category_id == test){
+            // $("#category_id").show();
+        // } else {
+            // $("#rationcard_no").hide();
+        // }
+    // }
+
+	$("#category").change(function(){
+		$(this).val().show();
+	    // populate_tests($(this).val())
+	})
+)});
